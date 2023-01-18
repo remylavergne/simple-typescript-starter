914677d489d9cb875495666a9610de0e18134141
Rémy LAVERGNE-PRUDENCE <lavergne.remy@gmail.com>
2023-01-18 11:54:17 +0100

    add rxjs + simple code

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
+import { delay, from, Observable } from 'rxjs';
+
 // eslint-disable-next-line no-console
-console.log('Hello world!');
+console.log('RxJS TS Playground');
+
+const data: number[] = [1, 2, 3, 4, 5];
+
+from(data).subscribe((d: number) => console.log('Data received:', d));
