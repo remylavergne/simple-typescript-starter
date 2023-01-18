353df64aca6150514ba61cf812c9efa1ba3ef697
Rémy LAVERGNE-PRUDENCE <lavergne.remy@gmail.com>
2023-01-18 13:18:54 +0100

    refactor example

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
-import { delay, from, Observable } from 'rxjs';
+import { from } from 'rxjs';
 
 // eslint-disable-next-line no-console
-console.log('RxJS TS Playground');
+console.log(`------------------
+RxJS TS Playground
+------------------\n`);
 
-const data: number[] = [1, 2, 3, 4, 5];
-
-from(data).subscribe((d: number) => console.log('Data received:', d));
+from([1, 2, 3, 4, 5]).subscribe((d: number) =>
+  // eslint-disable-next-line no-console
+  console.log('Data received:', d),
+);
